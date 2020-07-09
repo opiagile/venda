47cf8bb0a414fcd93753d76d23102f19ff76ce19
Francisco Araujo <francisco@opiagile.com>
2020-07-08 23:44:30 -0300

    criando respository e service para Categoria

---
+++ src/main/java/com/opiagile/venda/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.opiagile.venda.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.opiagile.venda.domain.Categoria;
+import com.opiagile.venda.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar( Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
