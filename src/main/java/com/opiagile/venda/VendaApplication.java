1bebee50284a5ff0d41e9dcab4e2b6f50162e64e
Francisco Araujo <francisco@opiagile.com>
2020-07-08 23:54:06 -0300

    Criando operação de instanciação

--- src/main/java/com/opiagile/venda/VendaApplication.java
+++ src/main/java/com/opiagile/venda/VendaApplication.java
@@ -1,13 +1,31 @@
 package com.opiagile.venda;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class VendaApplication {
+import com.opiagile.venda.domain.Categoria;
+import com.opiagile.venda.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class VendaApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2)); 
 	}
 
 }
