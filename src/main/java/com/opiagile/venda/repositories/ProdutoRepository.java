bda097b5113b1483688c8b29d5513ac2796c3d82
Francisco Araujo <francisco@opiagile.com>
2020-07-10 00:05:08 -0300

    Implementação Estado e Cidade

--- src/main/java/com/opiagile/venda/repositories/ProdutoRepository.java
+++ src/main/java/com/opiagile/venda/repositories/ProdutoRepository.java
@@ -3,7 +3,6 @@ package com.opiagile.venda.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.opiagile.venda.domain.Categoria;
 import com.opiagile.venda.domain.Produto;
 
 @Repository
